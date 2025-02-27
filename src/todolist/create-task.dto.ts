c1ad1a50ef7478d39d96145764635561b33f92a6
Manilka Keshan Abeygunaratna <manilkakeshan1000@gmail.com>
2025-02-27 23:06:52 +0530

    initialized entity and dto

---
+++ src/todolist/create-task.dto.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty, IsBoolean, IsString } from 'class-validator';
+
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  date: string;
+
+  @IsNotEmpty()
+  @IsString()
+  time: string;
+
+  @IsBoolean()
+  isRecurring: boolean;
+}
