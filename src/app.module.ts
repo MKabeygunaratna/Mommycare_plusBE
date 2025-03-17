0d3b9a5563c3c78e4c0b56cba82650518c649f4f
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2025-03-17 13:35:14 +0530

    Complete edition

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuizModule } from './quiz/quiz.module';
 import { EmailModule } from './email/email.module'; // Import EmailModule
 import { ChatbotModule } from './chatbot/chatbot.module';
+import { ChatbotVoiceModule } from './chatbotvoice/chatbotvoice.module';
 
 
 @Module({
-  imports: [QuizModule, EmailModule,ChatbotModule],  // Include EmailModule here
+  imports: [QuizModule, EmailModule,ChatbotVoiceModule,ChatbotModule],  // Include EmailModule here
 })
 export class AppModule {}
--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
-import * as multer from 'multer';  
 import { ChatbotController } from './chatbot.controller';
 import { ChatbotService } from './chatbot.service';
 
 @Module({
-  imports: [
-    MulterModule.register({
-      storage: multer.memoryStorage(),
-    }),
-  ],
   controllers: [ChatbotController],
   providers: [ChatbotService],
+  exports: [ChatbotService]
 })
 export class ChatbotModule {}
