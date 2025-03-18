0a01e214d3850ca34daa450b8445da077f54f024
NotSithuwa <ishamyafernando@gmail.com>
2025-03-18 10:23:27 +0530

    google fit

--- src/config/google-fit.config.ts
+++ src/config/google-fit.config.ts
@@ -1,5 +1,5 @@
 export const googleFitConfig = {
-  clientId: 'YOUR_GOOGLE_CLIENT_ID',
-  clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
-  redirectUri: 'YOUR_REDIRECT_URI',
+  clientId: 'Y476115653187-3hrfto1aeu1nf9ilad1oor4gp3pmevjd.apps.googleusercontent.com',
+  clientSecret: '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',
+  redirectUri: 'http://localhost:3000/auth/google/callback',
 };
